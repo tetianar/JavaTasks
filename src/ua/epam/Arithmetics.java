e52a498b602382a7fb2ce4ef057bc6f273d1cfac
Tetiana <Tetiana_Romaniv@epam.com>
2016-09-13 14:51:35 +0300

    add class Arithmetics

---
+++ src/ua/epam/Arithmetics.java
@@ -0,0 +1,16 @@
+package ua.epam;
+
+public class Arithmetics {
+
+    public double add (double a, double b){
+        return a + b;
+    }
+
+    public double deduct (double a, double b){
+        return a - b;
+    }
+
+    public double mult (double a, double b){
+        return a * b;
+    }
+}
